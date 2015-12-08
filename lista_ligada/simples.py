b65ff8994502b67fbb6a7ec2bf22c03a4eb125d6
renzon <renzon@gmail.com>
2015-12-08 00:49:43 -0200

    Teste para adição de um elemento

--- lista_ligada/simples.py
+++ lista_ligada/simples.py
@@ -9,7 +9,7 @@ class Nó:
 
 class ListaLigadaSimples:
     def __init__(self):
-        self.inicio = None  #aponta para o Nó inicial
+        self.inicio = None  # aponta para o Nó inicial
         self.tamanho = 0
 
 
@@ -18,3 +18,10 @@ class TestesDeAdicaoEmListaSimples(TestCase):
         lista = ListaLigadaSimples()
         self.assertIsNone(lista.inicio)
         self.assertEqual(0, lista.tamanho)
+
+    def testar_adiciao_de_um_elemento(self):
+        lista = ListaLigadaSimples()
+        adicionar(lista, 'A')
+        noh_inicial = lista.inicio
+        self.assertIsNotNone(noh_inicial)
+        self.assertEqual('A', noh_inicial.valor)
